1766cb6c314ea965935fd32d5c3d9348a29e929a
johncook <jonnymcook1@gmail.com>
2019-06-10 17:20:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './Reset.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      menuStatus: 'drop-down-menu',
+      buttonStatus: 'drop-down-buttons'
+    }
+  }
+
+  handleClick = () => {
+    if(this.state.menuStatus === 'drop-down-menu-open') {
+      this.setState({menuStatus: 'drop-down-menu-closed'});
+    } else {
+      this.setState({menuStatus: 'drop-down-menu-open'});
+    }
+  } 
+
+  render() {
+    return (
+      <body className="App">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script&display=swap" rel="stylesheet"></link>
+          <nav>
+            <ul>
+              <div>
+                <li className='logo'>Start Bootstrap</li>
+              </div>
+              <li className='menu-text'>SERVICES</li>
+              <li className='menu-text'>PORTFOLIO</li>
+              <li className='menu-text'>ABOUT</li>
+              <li className='menu-text'>TEAM</li>
+              <li className='menu-text'>CONTACT</li>
+              <li>
+                <img onClick={this.handleClick}
+                src='https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png' alt='menu-bar'/>
+              </li>
+              <div className={this.state.menuStatus}>
+                
+              </div>
+              
+            </ul>
+          </nav>
+          <article>
+            <h2>Welcome To Our Studio!</h2>
+          </article>
+      </body>
+    );
+  }
 }
 
 export default App;
